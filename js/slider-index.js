2bcad78b2f6148afd8510659efa65f7966d0b726
Javier Castaños <javocz.10@gmail.com>
2021-05-10 15:30:00 -0300

    Career content + some styles and functions

--- js/slider-index.js
+++ js/slider-index.js
@@ -2,22 +2,26 @@ function changeSlide() {
 
     var imagenes=[
         {
-            "img":"./img/slider-index/slide-1.jpg",
+            "imgDsk":"./img/slider-index/slide-1.jpg",
+            "imgMb":"./img/slider-index/slide-3.jpg",
             "title":"Titulo 1",
             "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa quisquam et aspernatur quasi enim ratione."
         },
         {
-            "img":"./img/slider-index/slide-2.jpg",
+            "imgDsk":"./img/slider-index/slide-2.jpg",
+            "imgMb":"./img/slider-index/slide-4.jpg",
             "title":"Titulo 2",
             "description": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rem nam perspiciatis odio, fugit vel sequi. Dicta quisquam ducimus ut animi?"
         },
         {
-            "img":"./img/slider-index/slide-3.jpg",
+            "imgDsk":"./img/slider-index/slide-1.jpg",
+            "imgMb":"./img/slider-index/slide-3.jpg",
             "title":"Titulo 3",
             "description": "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Saepe, repellendus. Expedita, veritatis."
         },
         {
-            "img":"./img/slider-index/slide-4.jpg",
+            "imgDsk":"./img/slider-index/slide-2.jpg",
+            "imgMb":"./img/slider-index/slide-4.jpg",
             "title":"Titulo 4",
             "description": "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Distinctio fuga, quis dicta qui nam saepe maxime tempore vero dolores hic, repudiandae impedit ut doloremque ipsam."
         }
@@ -28,12 +32,19 @@ function changeSlide() {
 
     var i = 0;
     var time = 5000;
+    var deviceWidth = window.innerWidth;
     
     function changeData(){
 
-        document.sliderIndexImg.src = imagenes[i].img;
-        document.getElementById("sliderDescriptionTitle").innerHTML = imagenes[i].title;
-        document.getElementById("sliderDescriptionText").innerHTML = imagenes[i].description;
+        if (deviceWidth > 780){
+            document.sliderIndexImg.src = imagenes[i].imgMb;
+            document.getElementById("sliderDescriptionTitle").innerHTML = imagenes[i].title;
+            document.getElementById("sliderDescriptionText").innerHTML = imagenes[i].description;
+        }else {
+            document.sliderIndexImg.src = imagenes[i].imgDsk;
+            document.getElementById("sliderDescriptionTitle").innerHTML = imagenes[i].title;
+            document.getElementById("sliderDescriptionText").innerHTML = imagenes[i].description;
+        }
         
 
         if ( i < 3 ) {
